512c5de67f5081720a7ca42c7c9eea3e2e4293a3
3nd3r1 <ender.spam.guru@gmail.com>
2026-09-09 15:40:35 +0300

    chore: add a shell.nix for the nixers

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    sqlite
+    python3
+    python3Packages.pip
+    python3Packages.matplotlib
+    python3Packages.numpy
+    python3Packages.pandas
+    python3Packages.networkx
+    python3Packages.seaborn
+    python3Packages.scipy
+    python3Packages.scikit-learn
+    python3Packages.nltk
+  ];
+}
